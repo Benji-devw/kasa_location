558c17b4c141b8508bbfd71c2acbb1ba3d08d3a0
Benji-devw <ben.nav@pm.me>
2024-04-26 11:05:51 +0200

    fix/404.js Layout props page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <Layout>
+    <Layout page={"404"}>
       <div className="not-found">
         <h1>404</h1>
         <span>Oups! La page que vous demandez n'existe pas.</span>
