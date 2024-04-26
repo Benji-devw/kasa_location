6f930dcbaf4faa29181994b04c08041cd7cc476d
Benji-devw <ben.nav@pm.me>
2024-04-26 10:19:41 +0200

    fix/meta in Layout.js with react-helmet-async

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,25 @@
 import React from "react";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
 //TODO: add head
 const Layout = ({ children, page }) => {
   return (
-    <React.Fragment>
-      <head>
+    <HelmetProvider>
+      <Helmet>
         <title>Kasa - {page}</title>
         <meta
           name="Kasa"
           content="Kasa est dans le métier de la location d’appartements entre particuliers depuis près de 10 ans maintenant"
         />
-      </head>
+      </Helmet>
       <div className="container">
         <Header />
         <main>{children}</main>
         <Footer />
       </div>
-    </React.Fragment>
+    </HelmetProvider>
   );
 };
 export default Layout;
